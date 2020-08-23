2c06cc501da4a59282df373bd73fd2428dadb9d3
4krajesh <4k.rajesh@gmail.com>
2020-08-23 19:10:48 +0530

    view as cards

--- jp-poultry/src/components/main.js
+++ jp-poultry/src/components/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button'
+import { Card, Button } from 'react-bootstrap';
 
 class Main extends Component {
 	constructor(props) {
@@ -8,7 +8,18 @@ class Main extends Component {
   }
 	render() {
 		const listSheds = this.state.sheds.map((shed, index) =>
-			<Button variant="light" href={`/shed/${shed.name}`} key={index}>{shed.name}</Button>
+			<Card style={{ width: '18rem' }} key={index}>
+  <Card.Body>
+    <Card.Title>Shed Name</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">Shed ID</Card.Subtitle>
+    <Card.Text>
+			<p>Start Date: </p>
+			<p>End Date:</p>
+			<p>Current Week:</p>
+    </Card.Text>
+    <Card.Link href={`/shed/${shed.name}`}>Show details</Card.Link>
+  </Card.Body>
+</Card>
 		);
 		return (
                 <div>
